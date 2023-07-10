28d62d0000b5d21815057ab8c7f2eace7c112ae6
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2023-07-10 22:32:25 +0200

    Update with try and except

--- Small_projects/grosspay.py
+++ Small_projects/grosspay.py
@@ -1,11 +1,15 @@
-hours = float(input("Enter Hours: "))
-rate = float(input("Enter Rate: "))
-pay = round(hours * rate, 2)
-overpay = round(40 * rate + (hours - 40)  * rate * 1.5, 2)
+try:
+    hours = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate: "))
+except ValueError:
+  print("Error, please enter numeric input for Rate")
+  quit()
+else:  
+    pay = round(hours * rate, 2)
+    overpay = round(40 * rate + (hours - 40)  * rate * 1.5, 2)
 
 
-if hours <= 40:
-   print(f"Pay: {pay}")
-else:
-   print(f"Pay: {overpay}")
-  
+    if hours <= 40:
+       print(f"Pay: {pay}")
+    else:
+       print(f"Pay: {overpay}")
