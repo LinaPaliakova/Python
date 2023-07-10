ed075291f237b4b3417199af33f94b49f5a2975b
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2023-07-10 21:03:05 +0200

    Update grosspay.py

--- Small_projects/grosspay.py
+++ Small_projects/grosspay.py
@@ -1,4 +1,11 @@
 hours = float(input("Enter Hours: "))
 rate = float(input("Enter Rate: "))
 pay = round(hours * rate, 2)
-print(f"Pay: {pay}")
+overpay = round(40 * rate + (hours - 40)  * rate * 1.5, 2)
+
+
+if hours <= 40:
+   print(f"Pay: {pay}")
+else:
+   print(f"Pay: {overpay}")
+  
