eff55b901dd719f861600750aaa0282a92fdcdcc
LinaPaliakova <50527882+LinaPaliakova@users.noreply.github.com>
2023-07-12 14:23:52 +0200

    Update grosspay.py

--- Small_projects/grosspay.py
+++ Small_projects/grosspay.py
@@ -1,15 +1,25 @@
-try:
-    hours = float(input("Enter Hours: "))
-    rate = float(input("Enter Rate: "))
-except ValueError:
-  print("Error, please enter numeric input for Rate")
-  quit()
-else:  
+def compute_pay(hours,rate):
     pay = round(hours * rate, 2)
     overpay = round(40 * rate + (hours - 40)  * rate * 1.5, 2)
-
-
     if hours <= 40:
-       print(f"Pay: {pay}")
+       return f"Pay: {pay}"
     else:
-       print(f"Pay: {overpay}")
+       return f"Pay: {overpay}"
+
+
+def check_value(p_input):
+    try:
+        val = float(p_input)
+        return val
+    except ValueError:
+        print("Error, please enter numeric input for Rate")
+        quit()
+    
+
+
+hours = input("Enter Hours: ")
+hours = check_value(hours)
+rate = input("Enter Hours: ")
+rate = check_value(rate)
+
+print(compute_pay(hours,rate)) 
